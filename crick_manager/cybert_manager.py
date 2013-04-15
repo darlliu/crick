2aa07bab7c71c87675970b2bda3beb4249594de9
darlliu <darlliu@lavabit.com>
2013-04-14 18:27:01 -0700

    can now output cybert tables

--- crick_manager/cybert_manager.py
+++ crick_manager/cybert_manager.py
@@ -15,7 +15,7 @@ LABELS="(Sample TEXT, ProbeID INT, GeneSym TEXT, GeneDescription TEXT,GeneID TEX
 BASEDIR="/home/yul13/data/template/"
 CURDIR=os.getcwd();
 from cybert_slaves import *
-
+VERBOSE=0
 ARGS={"FNAME":"","CONTROL":"-1","EXPERIMENT":"-1", "BACKGROUND":"8", "WINDOWSIZE":"101",\
         "INTC":"T","INTE":"T","DOMULT":"T","PPDE":"F","DOQC":"F"}
 
@@ -48,6 +48,10 @@ class cybert_manager(object):
             print "Raw data not found, abort!"
             raise IOError;
         return
+    def pickle(self):
+        f=open(self.name+".cpkl","wb")
+        pickle.dump(self, f);
+        return;
 
     def gen_ord(self):
         self.RUNNAMES=[self.rawname.strip(".xls")]
@@ -55,7 +59,6 @@ class cybert_manager(object):
         self.ORDER=[["C0" for i in xrange(self.numsam/2)]+["E0" for i in xrange(self.numsam/2)]]
         print self.RUNNAMES, self.ORDER, self.RUNS
         return
-
 
     def load_ord(self,name):
         f=open("order.list","r")
@@ -96,7 +99,6 @@ class cybert_manager(object):
         """Make individual CyberT files."""
         controls=[i for i in xrange(len(self.order)) if self.order[i]=="C"+str(run_num)]
         exps=[i for i in xrange(len(self.order)) if self.order[i]=="E"+str(run_num)]
-        print controls, exps
         g=open(str(run_num)+".csv","w")
         g.write("\t".join(["ProbeID"]+[
             self.sams[i] for i in controls+exps
@@ -139,15 +141,76 @@ class cybert_manager(object):
                 f.write(jobtemplate.format(str(i)+".R",self.run_name))
                 f.close();
                 bashtemp+="qsub ./{0}\n".format(str(i)+".sge");
+                if self.runs==0:
+                    break;
             f=open("jobs.sh","w")
             f.write(bashtemp);
             f.close();
             os.chdir(CURDIR)
         return;
+    def write_output(self):
+        """given output and idconversion file, rejoin the table into more readable form"""
+        try:
+            os.mkdir("./CyberT_Output")
+        except:
+            pass;
+        f=open("./idConversions.csv","r")
+        lookup={};
+        for line in f:
+            break;
+        for line in f:
+            ids=line.strip().split("\t")
+            try:
+                lookup[ids[0]]
+                continue;
+            except KeyError:
+                lookup[ids[0]]=[ids[1],ids[2],ids[3]]
+            finally:
+                pass;
+        for i in xrange(len(self.RUNNAMES)):
+            RUNNAME=self.RUNNAMES[i]
+            self.order=self.ORDER[i]
+            self.runs=self.RUNS[i]
+            os.chdir(RUNNAME)
+            #these are assumed valid
+            for i in xrange(self.runs+1):
+                #load output file
+                myprefix=str(i)+".csv"
+                f=open(myprefix+"cybert.out","r").readlines()
+                g=open(CURDIR+"/CyberT_Output/"+self.run_name+"_"+RUNNAME+str(i)+".txt","w")
+                g.write("\t".join(["probe_id","gene_sym","gene_name","uniprot"]+f[0].split("\t")))
+                controls=[q for q in xrange(len(self.order)) if self.order[q]=="C"+str(i)]
+                cnt=0;
+                for temp,line  in zip(self.RAWDATA,f[1:]):
+                    #do a simple QC
+                    temp2=line.split("\t")
+                    try:
+                        a= "Found row with probe {0}, gene sym: {1} ".format(temp[0],lookup[temp[0]])
+                        if VERBOSE: print a;
+                        cnt+=1;
+                    except KeyError:
+                        if VERBOSE:
+                            print "Rows with no gene sym ignored {0}".format(temp[0])
+                        continue;
+                    assert (temp[controls[0]+1]==temp2[0])
+                    g.write("\t".join([temp[0]]+lookup[temp[0]]+temp2))
+                print "Done merging {0} with {1:d} rows".format(myprefix, cnt)
+                g.close()
+                print "purging large files with prefix",myprefix
+                os.remove(myprefix)
+                os.remove(myprefix+"cybert.out")
+
+            os.chdir(CURDIR)
+
+    def make_collections(self,pfx="CD271"):
+        """load the output file and make them into cybert_collection objects for later use"""
+        pass;
+    def serialize(self):
+        """serialize all cyber-t collections into databases"""
 
 def main():
     c=cybert_manager()
-    c.write_runs()
+    c.write_output()
 
 
 if __name__=="__main__":
